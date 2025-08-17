687e02866d8290f5eb1a31e2bb14e5fddc2d32d5
horizon <rekka728@gmail.com>
2025-08-18 07:50:13 +0900

    [add] scripts for convert image to bmp

---
+++ tools/make_bmp.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+FILE_ABSOLUTE_PATH=$(find `pwd` -name $1)
+FILE_ABSOLUTE_DIR=$(dirname $FILE_ABSOLUTE_PATH)
+FILE_NAME=$(basename $1 | cut -f 1 -d '.')
+
+if [ "$(uname)" == 'Darwin' ]; then
+    sips $1 -s format bmp --out ${FILE_ABSOLUTE_DIR}/${FILE_NAME}.bmp
+else
+    magick $1 ${FILE_NAME}.bmp
+fi
